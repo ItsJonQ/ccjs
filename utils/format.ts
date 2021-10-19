fa817c40b20c4b97e78bdf8ec7cdc14080e83bab
Jon Q <hello@jonquach.com>
2021-10-19 07:13:49 -0400

    Improve

--- utils/format.ts
+++ utils/format.ts
@@ -1,12 +1,29 @@
 import prettier from 'prettier/standalone'
 import parserBabel from 'prettier/parser-babel'
 import parserTypescript from 'prettier/parser-typescript'
+import parserMarkdown from 'prettier/parser-markdown'
 
 export function formatWithPrettier(value: string): string {
   try {
     const next = prettier.format(value, {
       parser: 'babel',
       plugins: [parserBabel, parserTypescript],
+      printWidth: 80,
+    })
+    return next
+  } catch (err) {
+    console.log(err)
+    return value
+  }
+}
+
+export function formatWithPrettierMarkdown(value: string): string {
+  try {
+    const next = prettier.format(value, {
+      parser: 'babel',
+      plugins: [parserBabel, parserMarkdown],
+      printWidth: 80,
+      proseWrap: 'always',
     })
     return next
   } catch (err) {
@@ -16,13 +33,12 @@ export function formatWithPrettier(value: string): string {
 
 export function formatWithJSComments(value: string): string {
   const rows = value.trim().split('\n')
-  const result = ['/**', ' * @example', ' *', ' * ```jsx']
+  const result = ['/**']
 
   rows.forEach((row) => {
     result.push(` * ${row}`)
   })
 
-  result.push(' * ```')
   result.push(' */')
 
   return result.join('\n').trim()
